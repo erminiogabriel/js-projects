b9350b57244b89f3ead491cedc758edc2cb4bd9a
Gabriel Erminio Machado <67374825+erminiogabriel@users.noreply.github.com>
2023-06-23 17:15:56 -0300

    Update linkedin url

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
     <p>Let&apos;s create something amazing together.</p>
     <div>
     <a target='_blank' href='https://github.com/erminiogabriel' rel="noreferrer"><BsGithub size={30}/></a>
-    <a target='_blank' href='https://www.linkedin.com/in/gabriel-machado-49958326b/' rel="noreferrer"><BsLinkedin size={30}/> </a>
+    <a target='_blank' href='https://www.linkedin.com/in/gabriel-erminio-machado/' rel="noreferrer"><BsLinkedin size={30}/> </a>
     <a href='mailto:gabriel.erminio02@gmail.com' rel="noreferrer"><FiMail size={30}/></a>
     </div>
     </StyledContactSection>
